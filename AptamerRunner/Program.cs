c075d1be23ebe666d3fc9e9db36e160051ad3b61
Victoria Shearer <V.shearer99@gmail.com>
2025-03-03 17:06:42 -0600

    Update Program.cs

--- AptamerRunner/Program.cs
+++ AptamerRunner/Program.cs
@@ -1,4 +1,4 @@
-﻿using AptamerRunner;
+using AptamerRunner;
 
 const int exitFailure = 1;
 
@@ -15,7 +15,7 @@ var arguments = Environment.GetCommandLineArgs().ToList();
 string workingDir = Path.GetFullPath(Path.GetDirectoryName(arguments[0])!);
 arguments.RemoveAt(0);
 
-if (arguments.Count > 0 && args[0] == "--DEBUG")
+if (arguments.Count > 0 && arguments[0] == "--DEBUG")
 {
     Debug.IsEnabled = true;
     arguments.RemoveAt(0);
@@ -24,43 +24,41 @@ if (arguments.Count > 0 && args[0] == "--DEBUG")
 if (Helpers.OsRequiresSudo())
 {
     Console.WriteLine(
-        "PLEASE NOTE: This script uses sudo to execute docker commands on your behalf. For this reason you will see password requests at times when executing this script.");
+        "PLEASE NOTE: This script uses sudo to execute singularity commands on your behalf. For this reason you will see password requests at times when executing this script.");
     Console.WriteLine();
 }
 
-
-if (!await Helpers.DockerExistsAsync())
+if (!await Helpers.SingularityExistsAsync())
 {
     Console.WriteLine(
-        "Docker is not installed or not accessible but is required by this script. Please download and install docker. See https://www.docker.com");
+        "Singularity is not installed or not accessible but is required by this script. Please download and install Singularity. See https://sylabs.io/singularity/");
     Environment.Exit(exitFailure);
     return; // Silence uninitialized warning
 }
 
-
 ImageInfo imageInfo;
 try
 {
-    imageInfo = Helpers.DockerImageInfo();
+    imageInfo = Helpers.SingularityImageInfo();
 }
 catch (Exception ex)
 {
-    Console.WriteLine("Failed to generate docker image name. Please get your environment's setup");
+    Console.WriteLine("Failed to generate singularity image name. Please check your environment's setup");
     Console.WriteLine(ex.Message);
     Environment.Exit(exitFailure);
     return; // Silence uninitialized warning
 }
 
-Console.WriteLine($"Using docker image {imageInfo.FullName()}\n");
+Console.WriteLine($"Using singularity image {imageInfo.FullName()}\n");
 
-Console.WriteLine("Pulling docker image to verify the latest version is present.\n");
+Console.WriteLine("Pulling singularity image to verify the latest version is present.\n");
 
 // We only need to run a pull if a repo has been specified
 if (!string.IsNullOrWhiteSpace(imageInfo.Repo))
 {
     if (!await Commands.PullImageIfMissingAsync(imageInfo))
     {
-        Console.WriteLine($"Failed to pull docker image {imageInfo.FullName()}");
+        Console.WriteLine($"Failed to pull singularity image {imageInfo.FullName()}");
         Environment.Exit(exitFailure);
         return; // Silence uninitialized warning
     }
@@ -93,7 +91,7 @@ switch (command)
     default:
         Console.WriteLine("ERROR: UNRECOGNIZED FIRST PARAMETER");
         Console.WriteLine(
-            $"This script accepts only '{Consts.Commands.PredictStructureCmd}', '{Consts.Commands.CreateGraphCmd}', and '{Consts.Commands.HelpCmd}' as it's first parameter");
+            $"This script accepts only '{Consts.Commands.PredictStructureCmd}', '{Consts.Commands.CreateGraphCmd}', and '{Consts.Commands.HelpCmd}' as its first parameter");
         break;
 }
 
